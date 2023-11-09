dd4d832acfdab6a4f0787e3f61d006dc50671eed
Anton <anton.horipavlovskyi.external@worldline.com>
2023-11-09 20:53:17 +0200

    fix xlsx parser

--- src/main/scala/com/devcode/accountiq/settlement/transformer/XLSXParser.scala
+++ src/main/scala/com/devcode/accountiq/settlement/transformer/XLSXParser.scala
@@ -2,12 +2,21 @@ package com.devcode.accountiq.settlement.transformer
 
 import org.apache.poi.ss.usermodel.{CellType, WorkbookFactory}
 
-import java.io.{File, FileInputStream}
+import java.io.FileInputStream
 import java.nio.file.Path
 import scala.jdk.CollectionConverters.asScalaIteratorConverter
 import zio._
 
+import java.text.{DecimalFormat, DecimalFormatSymbols}
+import java.util.Locale
+
 object XLSXParser {
+
+  // this is the best way how to convert doubles to strings and avoid values like 1.07794593E8 and etc
+  // for example: 12345678D.toString returns 1.2345678E7
+  // the solution from https://stackoverflow.com/a/25307973
+  val df = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH))
+  df.setMaximumFractionDigits(340) // 340 = DecimalFormat.DOUBLE_FRACTION_DIGITS
 
   def parse(path: Path): ZIO[Any, Nothing, List[List[String]]] = {
     val wb = WorkbookFactory.create(new FileInputStream(path.toFile))
@@ -23,7 +32,7 @@ object XLSXParser {
       }).flatten.map(row => row.cellIterator().asScala.toList.map { cell =>
       cell.getCellTypeEnum match {
         case CellType._NONE => ""
-        case CellType.NUMERIC => cell.getNumericCellValue.toString
+        case CellType.NUMERIC => df.format(cell.getNumericCellValue)
         case CellType.STRING => cell.getStringCellValue
         case CellType.FORMULA => cell.getCellFormula
         case CellType.BLANK => ""
